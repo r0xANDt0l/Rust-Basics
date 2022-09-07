21666fba837b95f9be59bac4775331423d26f5bd
rOxANDtOl <rodrigosgdlcc@gmail.com>
2022-09-07 14:05:33 +0200

    add all target folders, and rename variable project

---
+++ Common-Concepts/target/debug/deps/Variables-4873d11e5754f3af.d
@@ -0,0 +1,5 @@
+/home/rodriguetos/Github-Repos/Rust-Learning/Variables/target/debug/deps/Variables-4873d11e5754f3af: src/main.rs
+
+/home/rodriguetos/Github-Repos/Rust-Learning/Variables/target/debug/deps/Variables-4873d11e5754f3af.d: src/main.rs
+
+src/main.rs:
