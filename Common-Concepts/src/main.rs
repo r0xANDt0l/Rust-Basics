15a86327beaebee84d86f1c5fde00611b3f479fa
rOxANDtOl <rodrigosgdlcc@gmail.com>
2022-09-09 21:32:16 +0200

    started data tyes

--- Common-Concepts/src/main.rs
+++ Common-Concepts/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     // Variables
-    let mut x = 5; // Naming convention for variables -> all_like_this_with_underscores
+    let mut x = 5; // Naming convention for variables -> all_lowercase_with_underscores
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is: {x}");
@@ -21,5 +21,37 @@ fn main() {
         println!("The falue of y in the inner scope (Between the curly brackets) is {}" , y);
     }
 
-    println!("The value of y in the outside scope is {}", y)
+    println!("The value of y in the outside scope is {}", y);
+
+    // this will work
+
+    let spaces = "   ";
+    let spaces = spaces.len();
+
+    println!("there are {} spaces", spaces);
+
+    // this wont
+    /*
+    let mut spaces = "   ";
+    spaces = spaces.len();
+
+    println!("{spaces}");
+
+    It won't work because we're trying to change the variable type (String to int)
+    */
+
+    // ========== //
+    // Data types //
+    // ========== //
+
+    //Since `guess` can be any data type, we need to specify what kind is the number, since the rust compiler needs more info to know what we want it for
+
+    let guess : u8 = "42"
+        .parse()
+        .expect("Not a number!");
+    //this could be a single line, but it's  h a r d  t o  r e a d
+
+
+    println!("{guess}");
+
 }
