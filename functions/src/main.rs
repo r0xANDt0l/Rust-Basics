e496abe1258fe9ab8ca5097d38e065326fa372d9
rOxANDtOl <rodrigosgdlcc@gmail.com>
2022-09-13 22:03:55 +0200

    Functions!

---
+++ functions/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    hello_world();
+}
+
+// Functions: fn (snake_case name)() { }
+// They allow you to call it at any moment, and run a piece of it at any moment
+fn hello_world() {
+    println!("Hola Mundo!");
+}
