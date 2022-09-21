66f8b22cf0e9789a19446a041a6200eabaab466a
rOxANDtOl <rodrigosgdlcc@gmail.com>
2022-09-21 23:09:23 +0200

    rename

---
+++ control_flow/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    //Classic if
+    // lets you run code if a condition is true
+
+    let number: i32 = -10;
+
+    if number >= 10 {
+        println!("Number is more or equals to 10! The value of it is {number}");
+    } else {
+        println!("The number is less than 10!, The value of it is {number}");
+    }
+}
